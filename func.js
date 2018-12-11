7cfaa79927afc46f1601d1e0682375d43dc8fa22
Alexey Sveshnikov <mr.alyxey13@gmail.com>
2018-12-11 05:16:45 +0300

    fix

--- func.js
+++ func.js
@@ -2,11 +2,11 @@ function applyCSS(){
   $('.containertext').css('transform', 'rotateX(30deg) rotateZ(-30deg) rotateY(10deg) translate3d(-100px,'+ sc +'px,0px)');
   $('.container-origin').css('transform', 'rotate(0) translate3d(0px,'+ sc +'px,0px)');
 }
-var sc = 0;
+var sc = 150;
 
 $(window).on('mousewheel', function(event) {
     if(event.deltaY > 0){
-      if(sc < 0)
+      if(sc < 150)
         sc = sc + 100;
     }else{
       //if(sc > -2000)
